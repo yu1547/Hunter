1a9575088023bf0b2fd9c24627085c2e7e2f76a0
moxiowo <04150415betty@gmail.com>
2025-07-26 19:24:03 +0800

    settings
    音樂部分已完成

--- frontend/app/src/main/java/com/ntou01157/hunter/models/model_api/SettingsModel.kt
+++ frontend/app/src/main/java/com/ntou01157/hunter/models/model_api/SettingsModel.kt
@@ -2,7 +2,7 @@ package com.ntou01157.hunter.model.model_api
 
 import com.google.gson.annotations.SerializedName
 
-data class UserSettings(
+data class Settings(
     @SerializedName("music") val music: Boolean,
     @SerializedName("notification") val notification: Boolean,
     @SerializedName("language") val language: String
