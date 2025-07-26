c3cb81b295457331db9d382f47134c62fb47c57a
Uwusagi <64255522+flps1005121@users.noreply.github.com>
2025-07-26 21:54:23 +0800

    完成排行榜功能

--- frontend/app/src/main/java/com/ntou01157/hunter/models/rank.kt
+++ frontend/app/src/main/java/com/ntou01157/hunter/models/rank.kt
@@ -1,5 +1,12 @@
 package com.ntou01157.hunter.models
 
+data class RankItem( // 這個應該是後端 rankList 裡的每個項目
+    val userId: String,
+    val username: String,
+    val userImg: String?, // 注意：userImg 是可空的
+    val score: Int
+)
+
 data class RankResponse(
     val rankList: List<UserRanking>,
     val userRank: UserRanking? // 允許 userRank 為 null
